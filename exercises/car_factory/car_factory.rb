d82f99b6a1448efcd9054b1ed0643a3947947769
Luke Pawlik <lukepawlik@gmail.com>
2017-06-04 20:19:24 +0200

    CarFactory.new does allow creation of factory for supported brands only (Fiat, Lancia, Ford, Subaru)

--- exercises/car_factory/car_factory.rb
+++ exercises/car_factory/car_factory.rb
@@ -1,5 +1,15 @@
 class CarFactory
+
+  class UnsupportedBrandException < StandardError; end
+
+  SUPPORTED_BRANDS = %i{Fiat Lancia Ford Subaru}
+
   def initialize(name, brands: nil)
+    unless SUPPORTED_BRANDS.include?(brands.capitalize)
+      raise UnsupportedBrandException.new,
+            "Brand not supported: '#{brands.to_s.capitalize}'"
+    end
+
     @name = name
     @brads = brands
   end
