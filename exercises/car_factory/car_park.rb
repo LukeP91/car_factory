7c9c191792ff26199b0f5986ffbe6dda0a39650f
Luke Pawlik <lukepawlik@gmail.com>
2017-06-04 19:57:10 +0200

    CarPark#cars returns cars in car park

--- exercises/car_factory/car_park.rb
+++ exercises/car_factory/car_park.rb
@@ -1,8 +1,12 @@
 class CarPark
-  attr_reader :capacity
-  
+  attr_reader :capacity, :cars
+
   def initialize(capacity)
     @capacity = capacity
+    @cars = []
   end
 
+  def receive(cars)
+    @cars << cars
+  end
 end
