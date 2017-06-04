103cc595635b80f9455693d819b2cf0304d44ed4
Luke Pawlik <lukepawlik@gmail.com>
2017-06-04 19:48:41 +0200

    Car.new creates car instance of given supported color

--- exercises/car_factory/car.rb
+++ exercises/car_factory/car.rb
@@ -1,5 +1,10 @@
 class Car
-    def initialize(brand)
+  def initialize(brand, color: nil)
     @brand = brand
+    @color = color
+  end
+
+  def color_name
+    @color.to_s.split('_').map(&:capitalize).join(' ')
   end
 end
