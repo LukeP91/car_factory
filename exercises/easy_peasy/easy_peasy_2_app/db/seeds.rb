1da82364b6a18572e9244f1265b96a19912d51bc
Luke Pawlik <lukepawlik@gmail.com>
2017-06-11 23:24:29 +0200

    add seeds to populate database.

--- exercises/easy_peasy/easy_peasy_2_app/db/seeds.rb
+++ exercises/easy_peasy/easy_peasy_2_app/db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Job.create(name: "Inwentaryzacja", start_date: Date.new(2017,1,2), end_date: Date.new(2017,01,04))
+
+User.create(first_name: "John", last_name: "Doe", email: "john_doe@example.com")
+
+user_with_phone_not_accepted = User.create(first_name: "Josheph",
+                                           last_name: "Doe",
+                                           email: "joseph_doe@example.com")
+Profile.create(user_id: user_with_phone_not_accepted.id,
+               phone_number: "555123123",
+               accepted_at: nil)
+
+user_with_phone_accepted_1 = User.create(first_name: "John",
+                                         last_name: "John",
+                                         email: "john_john@example.com")
+Profile.create(user_id: user_with_phone_accepted_1.id,
+               phone_number: "555124124",
+               accepted_at: Date.current)
+
+user_with_phone_accepted_2 = User.create(first_name: "John",
+                                         last_name: "Does",
+                                         email: "john_does@example.com")
+Profile.create(user_id: user_with_phone_accepted_2.id,
+               phone_number: "555456445",
+               accepted_at: Date.current)
+Availability.create(user_id: user_with_phone_accepted_2.id,
+                    day_off: Date.new(2017,1,1))
+Availability.create(user_id: user_with_phone_accepted_2.id,
+                    day_off: Date.new(2017,1,5))
+
+user_with_phone_accepted_3 = User.create(first_name: "Paul",
+                                         last_name: "Doe",
+                                         email: "paul_doe@example.com")
+Profile.create(user_id: user_with_phone_accepted_3.id,
+               phone_number: "555789789",
+               accepted_at: Date.current)
+Availability.create(user_id: user_with_phone_accepted_3.id,
+                    day_off: Date.new(2017,1,2))
+Availability.create(user_id: user_with_phone_accepted_3.id,
+                    day_off: Date.new(2017,1,10))
