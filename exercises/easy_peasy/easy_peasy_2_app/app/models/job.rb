d97b8d8b2cd6cb129d0d190c6f4bf28ff9e27a61
Luke Pawlik <lukepawlik@gmail.com>
2017-06-12 20:46:21 +0200

    add Job#available_users.
    It returns all users that have accepted profile. And have no days off in the same time as given job.

--- exercises/easy_peasy/easy_peasy_2_app/app/models/job.rb
+++ exercises/easy_peasy/easy_peasy_2_app/app/models/job.rb
@@ -2,4 +2,11 @@ class Job < ActiveRecord::Base
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def available_users
+    job_date_range = start_date..end_date
+    users_with_day_off = User.with_accepted_profile.
+      with_day_off_in(job_date_range).map(&:id)
+    User.with_accepted_profile.where.not(id: users_with_day_off)
+  end
 end
--- exercises/easy_peasy/easy_peasy_2_app/app/models/user.rb
+++ exercises/easy_peasy/easy_peasy_2_app/app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  def self.with_accepted_profile
+    joins(:profile).where.not(profiles: { accepted_at: nil })
+  end
+
+  def self.with_day_off_in(date)
+    joins(:availabilities).where(availabilities: { day_off: date } )
+  end  
 end
