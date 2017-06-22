c2a1ba0c2bfce5634759caeb46f119a126955b03
Luke Pawlik <lukepawlik@gmail.com>
2017-06-22 20:29:11 +0200

    refactor some parts of car.rb according to suggestions.
    * removed nil from if statment in initialize
    * changed whole conditional in initialize to ternary operator

--- exercises/car_factory/car.rb
+++ exercises/car_factory/car.rb
@@ -3,12 +3,7 @@ class Car
 
   def initialize(brand, color: nil)
     @brand = brand
-    if color.nil?
-      @color = AVAILABLE_COLORS.first
-      AVAILABLE_COLORS.rotate!
-    else
-      @color = color
-    end
+    @color = color ? color : get_available_color
   end
 
   def color_name
@@ -17,5 +12,13 @@ class Car
 
   def brand
     @brand.to_s.capitalize
+  end
+
+  private
+
+  def get_available_color
+    color = AVAILABLE_COLORS.first
+    AVAILABLE_COLORS.rotate!
+    color
   end
 end
