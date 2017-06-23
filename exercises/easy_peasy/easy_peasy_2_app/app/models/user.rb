d93a1fac8161284562adc89f58008c7839902a55
Luke Pawlik <lukepawlik@gmail.com>
2017-06-23 18:13:37 +0200

    changed methods in User model to scopes.

--- exercises/easy_peasy/easy_peasy_2_app/app/models/user.rb
+++ exercises/easy_peasy/easy_peasy_2_app/app/models/user.rb
@@ -6,11 +6,9 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
 
-  def self.with_accepted_profile
-    joins(:profile).where.not(profiles: { accepted_at: nil })
-  end
+  scope :with_accepted_profile, -> { joins(:profile).where
+    .not(profiles: { accepted_at: nil }) }
 
-  def self.with_day_off_in(date)
-    joins(:availabilities).where(availabilities: { day_off: date } )
-  end  
+  scope :with_day_off_in, ->(date) { joins(:availabilities).
+    where(availabilities: { day_off: date } ) }
 end
